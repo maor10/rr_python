fcd0eb43feeda09ba2abd9871960a9b75963f57d
Maor Kern <kern@adaptive-shield.com>
2020-07-11 02:57:25 +0300

    Remove references to ipdb from pyrecorder for now

--- pyrecorder/src/pyrecorder/decorators.py
+++ pyrecorder/src/pyrecorder/decorators.py
@@ -12,9 +12,7 @@ def record(func):
     @wraps(func)
     def _wrapper(*args, **kwargs):
         if consts.REPLAY_SERVER_PROC_PATH.exists():
-            # import ipdb
             os.kill(os.getpid(), signal.SIGSTOP)
-            # ipdb.set_trace()
         else:
             start_recording()
         return func(*args, **kwargs)

3adcca5d0b079832b8d09009ed63f9d67efcef3a
Maor Kern <kern@adaptive-shield.com>
2020-07-22 01:35:26 +0300

    Remove random from client in pager

--- pager/src/pager/client.py
+++ pager/src/pager/client.py
@@ -30,25 +30,4 @@ def send_request_to_take_snapshot():
     return Client.create().send_request_to_take_snapshot()
 
 
-if __name__ == '__main__':
-    import random
-    print("generating random...")
-    x = []
-    for i in range(1000 * 10000):
-        x.append(random.randint(0, 100))
-    print("starting...")
-    # print(os.getpid())
-    # time.sleep(100000)
-    client = Client.create()
-    start_time = time.time()
-    client.send_request_to_take_snapshot()
-    end_time = time.time()
-    print(f"TIME {end_time - start_time}")
-    # Randomly print to file to restore acting appropriately
-    for i in range(30):
-        time.sleep(1)
-        with open('/tmp/play.txt', 'a') as f:
-            f.write(f'{i}\n')
-    print("Finished execution")
-
 

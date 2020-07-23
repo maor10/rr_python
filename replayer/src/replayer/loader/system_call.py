a2aa5c593259cdf628b7ccf91027b7d784dd62f4
Maor Kern <kern@adaptive-shield.com>
2020-07-23 21:12:59 +0300

    Remove irrelavnt utils module

--- replayer/src/replayer/loader/system_call.py
+++ replayer/src/replayer/loader/system_call.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
 from typing import List, Dict
 
-from replayer.system_consts import SYS_CALL_REGISTER
+from replayer.system_consts import SYS_CALL_REGISTER, SYS_CALL_NAMES
 from .memory_copy import MemoryCopy
-from replayer.utils import get_syscall_name_from_syscall_num
 from .registers import Registers
 
 
@@ -20,7 +19,7 @@ class SystemCall:
 
     @property
     def name(self) -> int:
-        return get_syscall_name_from_syscall_num(self.num)
+        return SYS_CALL_NAMES[self.num]
 
     def __repr__(self):
         return f"<{self.__class__.__name__} {self.name} -> {self.return_value}>"
--- replayer/src/replayer/utils.py
+++
@@ -1,5 +0,0 @@
-from replayer.system_consts import SYS_CALL_NAMES
-
-
-def get_syscall_name_from_syscall_num(syscall_num: int):
-    return SYS_CALL_NAMES[syscall_num]

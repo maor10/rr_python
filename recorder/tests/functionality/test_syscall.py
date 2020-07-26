05d23a51f2bb0ac3c6e5ba261c04de5cf19aa5df
Gilad <dkgs1998@gmail.com>
2020-07-27 00:37:36 +0300

    added more asserts

--- recorder/tests/functionality/test_syscall.py
+++ recorder/tests/functionality/test_syscall.py
@@ -4,13 +4,15 @@ import select
 def syscall_sum_copies(syscall):
     return sum(len(copy.buffer) for copy in syscall.memory_copies)
 
-def test_read_zero(kernel_module, record_syscalls_context, get_recorded_syscalls):
+def test_read(kernel_module, record_syscalls_context, get_recorded_syscalls):
     with open("/dev/urandom") as f:
         with record_syscalls_context():
-            os.read(f.fileno(), 100)
+            ret = os.read(f.fileno(), 100)
         
     syscalls = get_recorded_syscalls()
     
+    assert len(ret) == 100
+
     assert len(syscalls) == 1
     assert syscalls[0].name == "read"
     assert syscalls[0].return_value == 100
@@ -20,14 +22,16 @@ def test_write(kernel_module, record_syscalls_context, get_recorded_syscalls):
     
     with open("/dev/null", "wb") as f:
         with record_syscalls_context():
-            os.write(f.fileno(), b"F" * 100)
+            ret = os.write(f.fileno(), b"F" * 100)
     
     syscalls = get_recorded_syscalls()
 
+    assert ret == 100
+    
     assert len(syscalls) == 1
     assert syscalls[0].name == "write"
     assert syscalls[0].return_value == 100
-
+    assert syscall_sum_copies(syscalls[0]) == 0
 
 def test_poll(kernel_module, record_syscalls_context, get_recorded_syscalls):
     with open("/dev/urandom", "rb") as f:
